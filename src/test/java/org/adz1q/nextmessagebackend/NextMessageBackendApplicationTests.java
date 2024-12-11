3e4e6e3a8b93b5d75db9edb6d7a3ff86ce7fd27a
Adz1q <mikolajs2211@gmail.com>
2024-12-11 19:23:23 +0100

    added important files

---
+++ src/test/java/org/adz1q/nextmessagebackend/NextMessageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adz1q.nextmessagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextMessageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
