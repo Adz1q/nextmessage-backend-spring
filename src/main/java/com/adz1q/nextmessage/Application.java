c90b28314227853fdf6fddbaf3eb40323e0d3689
Adz1q <mikolajs2211@gmail.com>
2025-03-21 19:42:34 +0100

    Better validation in registering, loging in and fetching chats

--- src/main/java/com/adz1q/nextmessage/Application.java
+++ src/main/java/com/adz1q/nextmessage/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NextMessageApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(NextMessageApplication.class, args);
+        SpringApplication.run(Application.class, args);
 
         System.out.println("Hello Piesek!");
     }
